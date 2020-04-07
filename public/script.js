0a65274671eac35786bf2d53c70c94a236b4c79d
Glitch (hello-express) <none>
2020-04-07 09:58:30 +0000

    🛀🏃 Checkpoint ./public/script.js:3168504/626

--- public/script.js
+++ public/script.js
@@ -1,14 +1,29 @@
-document.getElementById('md-editor').onkeyup = function() {
+var editor = document.getElementById('md-editor');
+
+editor.onkeyup = function() {
   var md = this.value;
   console.log(md);
   document.getElementById("md-html").innerHTML = marked(md);
 }
 
-function insertAtCursor (el, value) {
-  el.focus();
+function insertAtCursor(value) {
+  editor.focus();
   document.execCommand('insertText', false /*no UI*/, value);
 }
 
 function insert(md) {
-  switch ()
+  switch (md) {
+    case "h1":
+      insertAtCursor('# ');
+    case "h2":
+      insertAtCursor('## ');
+    case "h3":
+      insertAtCursor('### ');
+    case "h4":
+      insertAtCursor('#### ');
+    case "h5":
+      insertAtCursor('##### ');
+    case "h6":
+      insertAtCursor('###### ');
+  }
 }
