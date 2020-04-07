56d7a7f4db6b5a8b89f56e15b921f97357688845
Glitch (hello-express) <none>
2020-04-07 10:21:27 +0000

    🌼📈 Checkpoint ./public/script.js:3168504/4319

--- public/script.js
+++ public/script.js
@@ -1,46 +1,89 @@
-var editor = document.getElementById('md-editor');
+// declaring the editor for everyone 
+var editor = document.getElementById("md-editor");
 
-function convert() {}
+// keyup function to trigger MD to HTML conversion
 editor.onkeyup = function() {
-  var md = this.value;
+  let md = this.value;
   console.log(md);
   document.getElementById("md-html").innerHTML = marked(md);
-}
+};
 
 // function insertAtCursor(value) {
 //  editor.focus();
 //  document.execCommand('insertText', false /*no UI*/, value);
 // }
 
+// Events for MD elements to insert
 function insert(md) {
-  
-  let charBehind;
-  let charAfter;
-  
+  var charBehind;
+  var charAfter;
+
   switch (md) {
     case "h1":
-      charBehind = '#'
-      charAfter = '';
+      charBehind = "#";
+      charAfter = "";
       break;
     case "h2":
-      charBehind = '##'
-      charAfter = '';
+      charBehind = "##";
+      charAfter = "";
       break;
     case "h3":
-      charBehind = '###'
-      charAfter = '';
+      charBehind = "###";
+      charAfter = "";
       break;
     case "h4":
-      charBehind = '####'
-      charAfter = '';
+      charBehind = "####";
+      charAfter = "";
       break;
     case "h5":
-      charBehind = '#####'
-      charAfter = '';
+      charBehind = "#####";
+      charAfter = "";
       break;
     case "h6":
-      charBehind = '######'
-      charAfter = ''
+      charBehind = "######";
+      charAfter = "";
       break;
+    case "bold":
+      charBehind = "**";
+      charAfter = "**";
+      break;
+    case "italic":
+      charBehind = "*";
+      charAfter = "*";
+      break;
+    case "strike":
+      charBehind = "~";
+      charAfter = "~";
+      break;
+    case "ul":
+      charBehind = "*";
+      charAfter = "";
+      break;
+    case "ol":
+      charBehind = "1.";
+      charAfter = "";
+    case "quote":
+      charBehind = "> ";
+      charAfter = "";
+      break;
+    case "inline":
+      charBehind = "`";
+      charAfter = "`";
+      break;
+    case "block":
+      charBehind = "```\n"
+      charAfter = "```";
   }
+
+  let start = editor.selectionStart; // editor start
+  let end = editor.selectionEnd; // editor end
+  editor.value =
+    editor.value.substring(0, start) +
+    charBehind +
+    editor.value.substring(start, end) +
+    charAfter +
+    editor.value.substring(end, editor.value.length);
+  editor.selectionStart = start + charBehind.length;
+  editor.selectionEnd = end + charBehind.length;
+  editor.focus(); // focus so as to not let the user lose concentration and return the input focus to editor
 }
