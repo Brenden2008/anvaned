1f3bb013ff312f50fc224536df36b32980b82672
Glitch (markme) <none>
2020-06-09 06:17:35 +0000

    💋🌔 Checkpoint ./public/script.js:3168504/14460 ./public/style.css:3168504/12224 ./views/index.html:3168504/14460

--- public/script.js
+++ public/script.js
@@ -1,20 +1,107 @@
-var app = new Vue({
-  el: "#app",
+Vue.use(VTooltip);
+
+Vue.component("markdown_el", {
+  template:
+    '<button :id="element" v-html="element" @click="insert(insertel)" v-tooltip="tooltip"></button>',
+  props: ["element", "insertel", "tooltip"],
+  methods: {
+    insert(md) {
+      var charBehind;
+      var charAfter;
+
+      switch (md) {
+        case "h1":
+          charBehind = "#";
+          charAfter = " ";
+          break;
+        case "h2":
+          charBehind = "##";
+          charAfter = " ";
+          break;
+        case "h3":
+          charBehind = "###";
+          charAfter = " ";
+          break;
+        case "h4":
+          charBehind = "####";
+          charAfter = " ";
+          break;
+        case "h5":
+          charBehind = "#####";
+          charAfter = " ";
+          break;
+        case "h6":
+          charBehind = "######";
+          charAfter = " ";
+          break;
+        case "bold":
+          charBehind = "**";
+          charAfter = "**";
+          break;
+        case "italic":
+          charBehind = "*";
+          charAfter = "*";
+          break;
+        case "strike":
+          charBehind = "~";
+          charAfter = "~";
+          break;
+        case "ul":
+          charBehind = "*";
+          charAfter = " ";
+          break;
+        case "ol":
+          charBehind = "1.";
+          charAfter = " ";
+        case "quote":
+          charBehind = "> ";
+          charAfter = " ";
+          break;
+        case "inline":
+          charBehind = "`";
+          charAfter = "`";
+          break;
+        case "block":
+          charBehind = "```\n";
+          charAfter = "\n```";
+          break;
+        case "hr":
+          charBehind = "---\n";
+          charAfter = "";
+          break;
+        case "a":
+          charBehind = "[";
+          charAfter = "]()";
+          break;
+        default:
+          console.error("Unknown markdown character!");
+      }
+
+      let editor = document.getElementById("md-editor");
+
+      let start = editor.selectionStart; // editor start
+      let end = editor.selectionEnd; // editor end
+      editor.value =
+        editor.value.substring(0, start) +
+        charBehind +
+        editor.value.substring(start, end) +
+        charAfter +
+        editor.value.substring(end, editor.value.length);
+      editor.selectionStart = start + charBehind.length;
+      editor.selectionEnd = end + charBehind.length;
+      editor.focus(); // focus so as to not let the user lose concentration and return the input focus to editor
+    }
+  }
+});
+
+Vue.component("theme", {
+  template: `<button id="toggle" @click="darktheme === true ? light() : dark()" v-tooltip="'Toggle dark/light mode'">🌙</button>`,
   data() {
     return {
-      md: localStorage.getItem("markdown") || "",
-      html: marked(localStorage.getItem("markdown")) || "",
-      darktheme: "false",
-      visible: false,
-      hidehtmltext: "Hide",
-      hidemdtext: "Hide"
+      darktheme: "false"
     };
   },
   methods: {
-    convert() {
-      this.html = marked(this.md);
-      localStorage.setItem("markdown", this.md); 
-    },
     light() {
       document.getElementById("toggle").innerText = "🌙";
       document.body.style.backgroundColor = "var(--background)";
@@ -73,93 +160,18 @@ var app = new Vue({
         p[i].style.color = "white";
       }
       this.darktheme = true;
-    },
-    insert(md) {
-      var charBehind;
-      var charAfter;
+    }
+  }
+});
 
-      switch (md) {
-        case "h1":
-          charBehind = "#";
-          charAfter = "";
-          break;
-        case "h2":
-          charBehind = "##";
-          charAfter = "";
-          break;
-        case "h3":
-          charBehind = "###";
-          charAfter = "";
-          break;
-        case "h4":
-          charBehind = "####";
-          charAfter = "";
-          break;
-        case "h5":
-          charBehind = "#####";
-          charAfter = "";
-          break;
-        case "h6":
-          charBehind = "######";
-          charAfter = "";
-          break;
-        case "bold":
-          charBehind = "**";
-          charAfter = "**";
-          break;
-        case "italic":
-          charBehind = "*";
-          charAfter = "*";
-          break;
-        case "strike":
-          charBehind = "~";
-          charAfter = "~";
-          break;
-        case "ul":
-          charBehind = "*";
-          charAfter = "";
-          break;
-        case "ol":
-          charBehind = "1.";
-          charAfter = "";
-        case "quote":
-          charBehind = "> ";
-          charAfter = "";
-          break;
-        case "inline":
-          charBehind = "`";
-          charAfter = "`";
-          break;
-        case "block":
-          charBehind = "```\n";
-          charAfter = "\n```";
-          break;
-        case "hr":
-          charBehind = "---\n";
-          charAfter = "";
-          break;
-        case "a":
-          charBehind = "[";
-          charAfter = "]()";
-          break;
-        default:
-          console.error("Unknown markdown character!");
-      }
-
-      let editor = document.getElementById("md-editor");
-
-      let start = editor.selectionStart; // editor start
-      let end = editor.selectionEnd; // editor end
-      editor.value =
-        editor.value.substring(0, start) +
-        charBehind +
-        editor.value.substring(start, end) +
-        charAfter +
-        editor.value.substring(end, editor.value.length);
-      editor.selectionStart = start + charBehind.length;
-      editor.selectionEnd = end + charBehind.length;
-      editor.focus(); // focus so as to not let the user lose concentration and return the input focus to editor
-    },
+Vue.component("download", {
+  template: `<button id="download" @click="visible === false ? show() : hide()" v-tooltip="'Download HTML or Markdown'">💾</button>`,
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
     show() {
       document.getElementById("down-pop").style.display = "block";
       this.visible = true;
@@ -167,7 +179,42 @@ var app = new Vue({
     hide() {
       document.getElementById("down-pop").style.display = "none";
       this.visible = false;
+    }
+  }
+});
+
+let localstorage = '';
+if (!localStorage.getItem("markdown")) {
+  localstorage = '';
+} else {
+  localstorage = localStorage.getItem("markdown");
+}
+
+Vue.component("mdhtml", {
+  template: `<div><textarea id="md-editor" v-model="md" v-on:keyup="convert"></textarea><div id="md-html" v-html="html"></div></div>`,
+  data() {
+    return {
+      md: localstorage || "",
+      html: marked(localstorage) || ""
+    };
+  },
+  methods: {
+    convert() {
+      this.html = marked(this.md);
+      localStorage.setItem("markdown", this.md);
     },
+  }
+})
+
+var app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      hidehtmltext: "Hide",
+      hidemdtext: "Hide"
+    };
+  },
+  methods: {
     download(filename, text) {
       var element = document.createElement("a");
       element.setAttribute(
