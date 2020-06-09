c5a4c84c096b1fb5c16d8875abc68937d939c26f
Glitch (markme) <none>
2020-06-09 06:06:10 +0000

    Rewound to commit 2cc8356606d47163b58c9c9f7598bd6134521634 + 0 edits

--- public/script.js
+++ public/script.js
@@ -1,107 +1,20 @@
-Vue.use(VTooltip);
-
-Vue.component("markdown_el", {
-  template:
-    '<button :id="element" v-html="element" @click="insert(insertel)" v-tooltip="tooltip"></button>',
-  props: ["element", "insertel", "tooltip"],
-  methods: {
-    insert(md) {
-      var charBehind;
-      var charAfter;
-
-      switch (md) {
-        case "h1":
-          charBehind = "#";
-          charAfter = " ";
-          break;
-        case "h2":
-          charBehind = "##";
-          charAfter = " ";
-          break;
-        case "h3":
-          charBehind = "###";
-          charAfter = " ";
-          break;
-        case "h4":
-          charBehind = "####";
-          charAfter = " ";
-          break;
-        case "h5":
-          charBehind = "#####";
-          charAfter = " ";
-          break;
-        case "h6":
-          charBehind = "######";
-          charAfter = " ";
-          break;
-        case "bold":
-          charBehind = "**";
-          charAfter = "**";
-          break;
-        case "italic":
-          charBehind = "*";
-          charAfter = "*";
-          break;
-        case "strike":
-          charBehind = "~";
-          charAfter = "~";
-          break;
-        case "ul":
-          charBehind = "*";
-          charAfter = " ";
-          break;
-        case "ol":
-          charBehind = "1.";
-          charAfter = " ";
-        case "quote":
-          charBehind = "> ";
-          charAfter = " ";
-          break;
-        case "inline":
-          charBehind = "`";
-          charAfter = "`";
-          break;
-        case "block":
-          charBehind = "```\n";
-          charAfter = "\n```";
-          break;
-        case "hr":
-          charBehind = "---\n";
-          charAfter = "";
-          break;
-        case "a":
-          charBehind = "[";
-          charAfter = "]()";
-          break;
-        default:
-          console.error("Unknown markdown character!");
-      }
-
-      let editor = document.getElementById("md-editor");
-
-      let start = editor.selectionStart; // editor start
-      let end = editor.selectionEnd; // editor end
-      editor.value =
-        editor.value.substring(0, start) +
-        charBehind +
-        editor.value.substring(start, end) +
-        charAfter +
-        editor.value.substring(end, editor.value.length);
-      editor.selectionStart = start + charBehind.length;
-      editor.selectionEnd = end + charBehind.length;
-      editor.focus(); // focus so as to not let the user lose concentration and return the input focus to editor
-    }
-  }
-});
-
-Vue.component("theme", {
-  template: `<button id="toggle" @click="darktheme === true ? light() : dark()" v-tooltip="'Toggle dark/light mode'">🌙</button>`,
+var app = new Vue({
+  el: "#app",
   data() {
     return {
-      darktheme: "false"
+      md: localStorage.getItem("markdown") || "",
+      html: marked(localStorage.getItem("markdown")) || "",
+      darktheme: "false",
+      visible: false,
+      hidehtmltext: "Hide",
+      hidemdtext: "Hide"
     };
   },
   methods: {
+    convert() {
+      this.html = marked(this.md);
+      localStorage.setItem("markdown", this.md); 
+    },
     light() {
       document.getElementById("toggle").innerText = "🌙";
       document.body.style.backgroundColor = "var(--background)";
@@ -160,18 +73,93 @@ Vue.component("theme", {
         p[i].style.color = "white";
       }
       this.darktheme = true;
-    }
-  }
-});
+    },
+    insert(md) {
+      var charBehind;
+      var charAfter;
 
-Vue.component("download", {
-  template: `<button id="download" @click="visible === false ? show() : hide()" v-tooltip="'Download HTML or Markdown'">💾</button>`,
-  data() {
-    return {
-      visible: false
-    };
-  },
-  methods: {
+      switch (md) {
+        case "h1":
+          charBehind = "#";
+          charAfter = "";
+          break;
+        case "h2":
+          charBehind = "##";
+          charAfter = "";
+          break;
+        case "h3":
+          charBehind = "###";
+          charAfter = "";
+          break;
+        case "h4":
+          charBehind = "####";
+          charAfter = "";
+          break;
+        case "h5":
+          charBehind = "#####";
+          charAfter = "";
+          break;
+        case "h6":
+          charBehind = "######";
+          charAfter = "";
+          break;
+        case "bold":
+          charBehind = "**";
+          charAfter = "**";
+          break;
+        case "italic":
+          charBehind = "*";
+          charAfter = "*";
+          break;
+        case "strike":
+          charBehind = "~";
+          charAfter = "~";
+          break;
+        case "ul":
+          charBehind = "*";
+          charAfter = "";
+          break;
+        case "ol":
+          charBehind = "1.";
+          charAfter = "";
+        case "quote":
+          charBehind = "> ";
+          charAfter = "";
+          break;
+        case "inline":
+          charBehind = "`";
+          charAfter = "`";
+          break;
+        case "block":
+          charBehind = "```\n";
+          charAfter = "\n```";
+          break;
+        case "hr":
+          charBehind = "---\n";
+          charAfter = "";
+          break;
+        case "a":
+          charBehind = "[";
+          charAfter = "]()";
+          break;
+        default:
+          console.error("Unknown markdown character!");
+      }
+
+      let editor = document.getElementById("md-editor");
+
+      let start = editor.selectionStart; // editor start
+      let end = editor.selectionEnd; // editor end
+      editor.value =
+        editor.value.substring(0, start) +
+        charBehind +
+        editor.value.substring(start, end) +
+        charAfter +
+        editor.value.substring(end, editor.value.length);
+      editor.selectionStart = start + charBehind.length;
+      editor.selectionEnd = end + charBehind.length;
+      editor.focus(); // focus so as to not let the user lose concentration and return the input focus to editor
+    },
     show() {
       document.getElementById("down-pop").style.display = "block";
       this.visible = true;
@@ -179,41 +167,7 @@ Vue.component("download", {
     hide() {
       document.getElementById("down-pop").style.display = "none";
       this.visible = false;
-    }
-  }
-});
-
-let localstorage = '';
-if (!localStorage.getItem("markdown")) {
-  localstorage = '';
-} else {
-  localstorage = localStorage.getItem("markdown");
-}
-
-Vue.component("mdhtml", {
-  template: `<div><textarea id="md-editor" v-model="md" v-on:keyup="convert"></textarea><div id="md-html" v-html="html"></div></div>`,
-  data() {
-    return {
-      md: localstorage || "",
-      html: marked(localstorage) || ""
-    };
-  },
-  methods: {
-    convert() {
-      this.html = marked(this.md);
-      localStorage.setItem("markdown", this.md);
     },
-  }
-})
-
-var app = new Vue({
-  el: "#app",
-  data() {
-    return {
-      
-    };
-  },
-  methods: {
     download(filename, text) {
       var element = document.createElement("a");
       element.setAttribute(
@@ -228,6 +182,28 @@ var app = new Vue({
       element.click();
 
       document.body.removeChild(element);
+    },
+    hidehtml() {
+      this.hidehtmltext = "Show";
+      document.getElementById("md-html").style.display = "none";
+      document.getElementById("md-editor").style.width = "100%";
+      document.getElementById("md-editor").style.borderRight = "none";
+      
+    },
+    showhtml() {
+      this.hidehtmltext = "Hide";
+      document.getElementById("md-html").style.display = "inline-block";  
+      document.getElementById("md-editor").style.width = "50%";
+      document.getElementById("md-editor").style.borderRight = "1px dashed var(--border)";
+    },
+    hidemd() {
+      this.hidemdtext = "Show";
+      
+      document.getElementById("md-editor").style.display = "none";
+    },
+    showmd() {
+      this.hidemdtext = "Hide";
+      document.getElementById("md-editor").style.display = "inline-block";
     }
   }
 });
