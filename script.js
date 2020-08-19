4b38015d63fc1fc51f53275761f1809550fb8b5b
Glitch (markme) <none>
2020-08-19 04:58:27 +0000

    🛳🌕 Checkpoint ./script.js:3168504/289

--- script.js
+++ script.js
@@ -328,13 +328,6 @@ var app = new Vue({
   }
 });
 
-let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
+   // so it can be date UI notify the user they can install the PWA
+  
