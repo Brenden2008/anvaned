592b2028c1627d475a6cdc49005bf6ee5b608bc9
Glitch (markme) <none>
2020-10-07 02:38:31 +0000

    🌉🍔 Checkpoint ./index.html:3168504/10 ./pwa.js:3168504/4

--- pwa.js
+++ pwa.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
   console.log("looking for worker")
-  navigator.serviceWorker.register('/pwa/sw.js')
+  navigator.serviceWorker.register('/sw.js')
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }
